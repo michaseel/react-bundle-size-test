b0f3cca75cdf2167e665245121e0ac11c94df7d3
mSeel <michaseel@users.noreply.github.com>
2018-11-20 17:05:51 +0100

    Updates

---
+++ precache-manifest.41cf3aba522f39bee1343b270e070280.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7fc4357574c3bc522ea",
+    "url": "/react-bundle-size-test/static/js/runtime~main.d7fc4357.js"
+  },
+  {
+    "revision": "51d21ea9af0cf9919d1e",
+    "url": "/react-bundle-size-test/static/js/main.51d21ea9.chunk.js"
+  },
+  {
+    "revision": "c2ea6c283125f00601cc",
+    "url": "/react-bundle-size-test/static/js/1.c2ea6c28.chunk.js"
+  },
+  {
+    "revision": "5b8f6b2e303f1fb4a6017021dc8c15de",
+    "url": "/react-bundle-size-test/index.html"
+  }
+];
